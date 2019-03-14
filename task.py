4400bf5eede4e4af87994ba9510b7d1641ce5c4d
wagnergarcez <30319795+wagnergarcez@users.noreply.github.com>
2019-03-14 09:48:58 -0300

    Update task.py
    Implementado uso do modulo 'urllib'

--- task.py
+++ task.py
@@ -2,37 +2,57 @@
 
 '''
     Author: Wagner Garcez
-    Date created: 13/03/2019
+    Date created: 14/03/2019
     Python Version: 3
 '''
 
+import sys
+import json
+
 try:
-    # import module
     import requests
+except:
+	import urllib.request
 
-    # variables
-    count = 0
-    all_websites = []
-    
-    # get request 
-    result = requests.get('https://jsonplaceholder.typicode.com/users')
-    
-    for data in result.json():
+def parse_json(json, all_websites, count):
 
-            # add websites in list
-            all_websites.append(data['website'])
+    for data in json:
 
-            # Samanthas's mail
-            if data['username'] == 'Samantha':
-                mail = data['email']
+        # add websites in list
+        all_websites.append(data['website'])
 
-            # counter of total users in the southern hemisphere
-            count += [lambda:0, lambda:1][float(data['address']['geo']['lat']) < 0]()
+        # Samanthas's mail
+        if data['username'] == 'Samantha':
+            mail = data['email']
+
+        # counter of total users in the southern hemisphere
+        count += [lambda:0, lambda:1][float(data['address']['geo']['lat']) < 0]()
+
+    return mail, count
+
+def main():
+
+	count = 0
+	all_websites = []
+
+	if 'requests' in sys.modules: # use module requests
+
+		res = requests.get('https://jsonplaceholder.typicode.com/users')
+		jdata = res.json() 
+
+	else: # use module urlib
+
+		url = urllib.request.urlopen('https://jsonplaceholder.typicode.com/users')
+		res = url.read()
+		jdata = json.loads(res.decode("utf-8"))
+
+	# call function 
+	mail, count = parse_json(jdata, all_websites, count)
 
     # outputs
-    print('\nWebsites de todos os usuarios: {} '.format(', '.join(map(str, all_websites))))
-    print('\nEmail da usuaria Samantha: {}'.format(mail))
-    print('\nTotal de usuarios no hemisferio sul: {}\n'.format(count))
+	print('\nWebsites de todos os usuarios: {} '.format(', '.join(map(str, all_websites))))
+	print('\nEmail da usuaria Samantha: {}'.format(mail))
+	print('\nTotal de usuarios no hemisferio sul: {}\n'.format(count))
 
-except Exception as e:
-    print (str(e))
+if __name__ == "__main__":
+	main()
